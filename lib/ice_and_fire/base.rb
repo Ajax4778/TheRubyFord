f81f5c8941cdc97784d67326be18aeb51755c130
Adithya Manohar <ajax4778@gmail.com>
2016-09-17 18:37:59 -0700

    check input attribute validity

--- lib/ice_and_fire/base.rb
+++ lib/ice_and_fire/base.rb
@@ -2,6 +2,8 @@ require 'active_support/inflector'
 
 class IceAndFire::Base
   def initialize(attributes)
+    raise "Invalid attributes for #{self.class}." unless valid?(attributes)
+
     method_names.each do |name|
       self.class.send(:attr_reader, name)
 
@@ -14,5 +16,9 @@ class IceAndFire::Base
 
   def method_names
     raise NotImplementedError
+  end
+
+  def valid?(attributes)
+    method_names == attributes.keys.sort.map { |k| k.underscore.to_sym }
   end
 end
