f9c2bbda88b61b5e47b8ac2415499c758435c18a
Adithya Manohar <ajax4778@gmail.com>
2016-10-02 21:21:01 -0700

    test cache clearing functionality

--- spec/ice_and_fire/cache_spec.rb
+++ spec/ice_and_fire/cache_spec.rb
@@ -2,7 +2,7 @@ require 'rspec'
 require 'the_ruby_ford'
 
 describe IceAndFire::Cache do
-  describe 'get' do
+  describe '#[]' do
     context 'if the requested object exists in the cache' do
       it 'returns the object' do
         api_object = { 'name' => 'Jon Snow' }
@@ -23,12 +23,20 @@ describe IceAndFire::Cache do
     end
   end
 
-  describe 'set' do
-    it 'adds the given object to the cache' do
-      api_object = { 'name' => 'Jon Snow' }
-      req_string = 'characters/583'
+  describe '#[]=' do
+    let(:api_object) { { 'name' => 'Jon Snow' } }
+    let(:req_string) { 'characters/583' }
 
+    it 'adds the given object to the cache' do
       cache = IceAndFire::Cache.new
+      cache[req_string] = api_object
+
+      expect(cache[req_string]).to eq(api_object)
+    end
+
+    it 'clears cache and adds object if cache reaches max size' do
+      cache = IceAndFire::Cache.new(a: 1, b: 2, c: 3)
+      stub_const('IceAndFire::Cache::DEFAULT_CACHE_LIMIT', 3)
       cache[req_string] = api_object
 
       expect(cache[req_string]).to eq(api_object)
